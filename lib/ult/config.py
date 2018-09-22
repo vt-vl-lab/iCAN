096f5f32e0d1359e2d7a97ee69aa64eb4844ba7c
gaochen315 <gaochen315@gmail.com>
2018-09-22 15:36:42 -0400

    Fix typo

--- lib/ult/config.py
+++ lib/ult/config.py
@@ -5,7 +5,7 @@
 # --------------------------------------------------------
 
 from __future__ import absolute_import
-from __future__ import divisions
+from __future__ import division
 from __future__ import print_function
 
 import os
